80194751e7c46a40328816694c939eac02982a60
Shumin Yoo <shuminyoo@Shumins-MacBook.local>
2022-03-02 09:58:24 +0900

    added mainpage file

--- aaron-app/src/App.js
+++ aaron-app/src/App.js
@@ -1,7 +1,16 @@
-
+import { Router, Route} from "react-router-dom";
 import LoginPage from "./LoginPage";
+import MainPage from "./MainPage";
+
 function App() {
-  return <LoginPage />
+  return ( 
+    <div>
+      <Router>
+       <LoginPage exact path ="/login" component = {LoginPage} />
+       <MainPage exact path ="/Main" component= {MainPage} />
+      </Router>
+    </div>
+    );
 }
 
 export default App;
---
+++ aaron-app/src/MainPage/index.js
@@ -0,0 +1,14 @@
+import styled from "styled-components";
+// import LoginPage from "../LoginPage";
+
+const TitleTest = styled.h1`
+    color:blue;
+`;
+
+function MainPage() {
+    return (
+        <TitleTest>Hello</TitleTest>
+    )
+}
+
+export default MainPage;
